7de63acf8fb81d443df9472537ed6b547a72ba07
suifeng123 <userna>
2017-08-08 18:04:42 +0800

    提交代码

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express'); //首先将这个框架印鉴来
-var router  = express.Router();
-
-router.get('/',function(req,res){
-  res.send('hello express');
-});
-
-module.exports = router;
+module.exports = function(app) {
+  app.get('/',function(req,res){
+    res.redirect('/posts');  //
+  });
+  app.use('/signup',require('./signup'));
+  app.use('/signin',require('./signin'));
+  app.use('/signout',require('./signout'));
+  app.use('/posts',require('./post'));
+}
